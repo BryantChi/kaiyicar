1d3abb3c5eebc0102419b577389fd15b59578cb6
BryantChi <bryantchi.work@gmail.com>
2021-01-04 04:05:48 +0800

    [MOD] 首頁畫面調整預設狀態

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -14,22 +14,33 @@ class HomeController extends Controller
     public function index(Content $content)
     {
         return $content
-            ->header('Dashboard')
-            ->description('Description...')
+            ->header('楷懿國際')
+            ->description('後台管理系統...')
             ->body(function (Row $row) {
-                $row->column(6, function (Column $column) {
-                    $column->row(Dashboard::title());
-                    $column->row(new Examples\Tickets());
+                // $row->column(6, function (Column $column) {
+                //     $column->row(Dashboard::title());
+                //     $column->row(new Examples\Tickets());
+                // });
+
+                // $row->column(6, function (Column $column) {
+                //     $column->row(function (Row $row) {
+                //         $row->column(6, new Examples\NewUsers());
+                //         $row->column(6, new Examples\NewDevices());
+                //     });
+
+                //     $column->row(new Examples\Sessions());
+                //     $column->row(new Examples\ProductOrders());
+                // });
+                $row->column(3, function (Column $column) {
+                    // $column->append(Dashboard::environment());
                 });
 
                 $row->column(6, function (Column $column) {
-                    $column->row(function (Row $row) {
-                        $row->column(6, new Examples\NewUsers());
-                        $row->column(6, new Examples\NewDevices());
-                    });
+                    $column->append('<div class="text-center" style="margin-top: 30%;margin-bottom: 30%;"><h1>歡迎使用楷懿國際後台管理系統</h1></div>');
+                });
 
-                    $column->row(new Examples\Sessions());
-                    $column->row(new Examples\ProductOrders());
+                $row->column(3, function (Column $column) {
+                    // $column->append(Dashboard::dependencies());
                 });
             });
     }
