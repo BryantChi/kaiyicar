6a37b65a1786887ba9630d56d117bb1cd4c290cb
BryantChi <bryantchi.work@gmail.com>
2021-07-13 21:01:03 +0800

    [MOD] 修正Model錯誤

--- app/Models/CatalogCarInfo.php
+++ app/Models/CatalogCarInfo.php
@@ -6,13 +6,10 @@ use Dcat\Admin\Traits\HasDateTimeFormatter;
 
 use Illuminate\Database\Eloquent\Model;
 
-use Spatie\EloquentSortable\Sortable;
-use Spatie\EloquentSortable\SortableTrait;
-
-class CatalogCarInfo extends Model implements Sortable
+class CatalogCarInfo extends Model
 {
     use HasDateTimeFormatter;
-    use SortableTrait;
+
     protected $table = 'catalog_car_infos';
 
     protected $casts = [
